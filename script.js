6cc79032a3f772cbb27073122debe6a5d4ad4050
Maurice Alves <maurice.estevam@gmail.com>
2021-01-03 21:41:44 -0300

    Requisito - 2.

--- script.js
+++ script.js
@@ -14,6 +14,27 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function addCart(idItem) {
+  fetch(`https://api.mercadolibre.com/items/${idItem.sku}`)
+  .then(response => response.json())
+  .then((data) => {
+    const add = {
+      sku: data.id,
+      name: data.title,
+      salePrice: data.price,
+    };
+    document.querySelector('.cart__items').appendChild(createCartItemElement(add));
+  });
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -21,8 +42,7 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')).addEventListener('click', () => { addCart({ sku }); });
   return section;
 }
 
@@ -40,18 +60,9 @@ window.onload = function onload() {
 };
 
 /* function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}*/
+    return item.querySelector('span.item__sku').innerText;
+  }*/
 
 /* function cartItemClickListener(event) {
   // coloque seu código aqui
-
-}*/
-
-/* function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }*/
